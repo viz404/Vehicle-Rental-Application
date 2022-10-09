019f69c6793fd266542e2ec66f36faebaed5ee95
viz404 <vishal.gxm@gmail.com>
2022-10-10 00:40:27 +0530

    data added for cars and bikes

---
+++ product-page/products-script.js
@@ -0,0 +1,139 @@
+let cars = [
+    {
+        "car-name": "Hyundai Eon (Petrol)",
+        "car-type": "Hatchback",
+        "transmission-type": "Manual",
+        "fuel-type": "Petrol",
+        "price": 2362,
+        "max-age": 21,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698293_eon.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Hyundai grand I10",
+        "car-type": "Hatchback",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 2572,
+        "max-age": 21,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700262_i10.png",
+        "seat": 5
+    },
+    {
+        "car-name": "TATA Tiago",
+        "car-type": "Hatchback",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 2572,
+        "max-age": 21,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700964_tiago.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Maruti Swift VDI (Diesel)",
+        "car-type": "Hatchback",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 2572,
+        "max-age": 21,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1562926309_suzuki_old.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Maruti Suzuki Baleno",
+        "car-type": "Hatchback",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 2762,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698059_baleno.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Maruti Suzuki Baleno (Petrol)",
+        "car-type": "Hatchback",
+        "transmission-type": "Manual",
+        "fuel-type": "Petrol",
+        "price": 2412,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698059_baleno.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Maruti Suzuki Celeio ZXI",
+        "car-type": "Hatchback",
+        "transmission-type": "Automatic",
+        "fuel-type": "Petrol",
+        "price": 2319,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698108_celerio.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Hyundai grand I10 (Automatic)",
+        "car-type": "Hatchback",
+        "transmission-type": "Automatic",
+        "fuel-type": "Petrol",
+        "price": 2585,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700262_i10.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Ford Aspire",
+        "car-type": "Sedan",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 2446,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700163_ford-aspire.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Brezza LDI",
+        "car-type": "SUV",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 2446,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558701126_vitara-breeza.png",
+        "seat": 5
+    },
+    {
+        "car-name": "Ford Ecosport Trend",
+        "car-type": "Mini SUV",
+        "transmission-type": "Manual",
+        "fuel-type": "Diesel",
+        "price": 3273,
+        "max-age": 60,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698257_eco-sport.png",
+        "seat": 5
+    }
+];
+let bikes = [
+    {
+        "bike-name": "Honda Activa",
+        "transmission-type": "Gearless",
+        "engine-capacity": 109,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1570607984_activa-4g.png"
+    },
+    {
+        "bike-name": "Honda Grazia",
+        "transmission-type": "Gearless",
+        "engine-capacity": 125,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1570608797_honnda-grazia.png"
+    },
+    {
+        "bike-name": "Honda CB Hornet 160R",
+        "transmission-type": "Geared",
+        "engine-capacity": 160,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593594_hornet.png"
+    },
+    {
+        "bike-name": "Honda XBlade",
+        "transmission-type": "Geared",
+        "engine-capacity": 162,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593537_honda-xblade.png"
+    }
+];
+
