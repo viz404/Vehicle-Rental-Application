b3326e759476faaf2a04f731d5e3bc69708745ad
viz404 <vishal.gxm@gmail.com>
2022-10-10 14:36:56 +0530

    able to fetch data and show

--- product-page/products-script.js
+++ product-page/products-script.js
@@ -1,139 +1,220 @@
 let cars = [
     {
-        "car-name": "Hyundai Eon (Petrol)",
-        "car-type": "Hatchback",
-        "transmission-type": "Manual",
-        "fuel-type": "Petrol",
+        "vehicle_name": "Hyundai Eon (Petrol)",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Manual",
+        "fuel_type": "Petrol",
         "price": 2362,
         "max-age": 21,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698293_eon.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 0.0
     },
     {
-        "car-name": "Hyundai grand I10",
-        "car-type": "Hatchback",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "Hyundai grand I10",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 2572,
         "max-age": 21,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700262_i10.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 0.0
     },
     {
-        "car-name": "TATA Tiago",
-        "car-type": "Hatchback",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "TATA Tiago",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 2572,
         "max-age": 21,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700964_tiago.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 0.0
     },
     {
-        "car-name": "Maruti Swift VDI (Diesel)",
-        "car-type": "Hatchback",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "Maruti Swift VDI (Diesel)",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 2572,
         "max-age": 21,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1562926309_suzuki_old.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 0.0
     },
     {
-        "car-name": "Maruti Suzuki Baleno",
-        "car-type": "Hatchback",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "Maruti Suzuki Baleno",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 2762,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698059_baleno.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 5.0
     },
     {
-        "car-name": "Maruti Suzuki Baleno (Petrol)",
-        "car-type": "Hatchback",
-        "transmission-type": "Manual",
-        "fuel-type": "Petrol",
+        "vehicle_name": "Maruti Suzuki Baleno (Petrol)",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Manual",
+        "fuel_type": "Petrol",
         "price": 2412,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698059_baleno.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 5.0
     },
     {
-        "car-name": "Maruti Suzuki Celeio ZXI",
-        "car-type": "Hatchback",
-        "transmission-type": "Automatic",
-        "fuel-type": "Petrol",
+        "vehicle_name": "Maruti Suzuki Celeio ZXI",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Automatic",
+        "fuel_type": "Petrol",
         "price": 2319,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698108_celerio.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 5.0
     },
     {
-        "car-name": "Hyundai grand I10 (Automatic)",
-        "car-type": "Hatchback",
-        "transmission-type": "Automatic",
-        "fuel-type": "Petrol",
+        "vehicle_name": "Hyundai grand I10 (Automatic)",
+        "vehicle_type": "Hatchback",
+        "transmission_type": "Automatic",
+        "fuel_type": "Petrol",
         "price": 2585,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700262_i10.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 5.0
     },
     {
-        "car-name": "Ford Aspire",
-        "car-type": "Sedan",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "Ford Aspire",
+        "vehicle_type": "Sedan",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 2446,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558700163_ford-aspire.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 5.0
     },
     {
-        "car-name": "Brezza LDI",
-        "car-type": "SUV",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "Brezza LDI",
+        "vehicle_type": "SUV",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 2446,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558701126_vitara-breeza.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 8.3
     },
     {
-        "car-name": "Ford Ecosport Trend",
-        "car-type": "Mini SUV",
-        "transmission-type": "Manual",
-        "fuel-type": "Diesel",
+        "vehicle_name": "Ford Ecosport Trend",
+        "vehicle_type": "Mini SUV",
+        "transmission_type": "Manual",
+        "fuel_type": "Diesel",
         "price": 3273,
         "max-age": 60,
         "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1558698257_eco-sport.png",
-        "seat": 5
+        "seat": 5,
+        "distance": 8.3
     }
 ];
 let bikes = [
     {
-        "bike-name": "Honda Activa",
-        "transmission-type": "Gearless",
-        "engine-capacity": 109,
-        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1570607984_activa-4g.png"
+        "vehicle_name": "Honda Activa",
+        "transmission_type": "Gearless",
+        "engine_capacity": 109,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1570607984_activa-4g.png",
+        "seat": 2,
+        "fuel_type": "Petrol",
+        "distance": 0,
+        "price": 903
     },
     {
-        "bike-name": "Honda Grazia",
-        "transmission-type": "Gearless",
-        "engine-capacity": 125,
-        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1570608797_honnda-grazia.png"
+        "vehicle_name": "Honda Grazia",
+        "transmission_type": "Gearless",
+        "engine_capacity": 125,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1570608797_honnda-grazia.png",
+        "seat": 2,
+        "fuel_type": "Petrol",
+        "distance": 5,
+        "price": 1032
     },
     {
-        "bike-name": "Honda CB Hornet 160R",
-        "transmission-type": "Geared",
-        "engine-capacity": 160,
-        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593594_hornet.png"
+        "vehicle_name": "Honda CB Hornet 160R",
+        "transmission_type": "Geared",
+        "engine_capacity": 160,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593594_hornet.png",
+        "seat": 2,
+        "fuel_type": "Petrol",
+        "distance": 6.2,
+        "price": 1693
     },
     {
-        "bike-name": "Honda XBlade",
-        "transmission-type": "Geared",
-        "engine-capacity": 162,
-        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593537_honda-xblade.png"
+        "vehicle_name": "Yamaha Fascino",
+        "transmission_type": "Geared",
+        "engine_capacity": 113,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593392_fascino.png",
+        "seat": 2,
+        "fuel_type": "Petrol",
+        "distance": 3.2,
+        "price": 903
+    },
+    {
+        "vehicle_name": "Honda XBlade",
+        "transmission_type": "Geared",
+        "engine_capacity": 162,
+        "img": "https://jtride-data.s3.ap-south-1.amazonaws.com/uploads/1560593537_honda-xblade.png",
+        "seat": 2,
+        "fuel_type": "Petrol",
+        "distance": 8.2,
+        "price": 1693
     }
 ];
 
+let data = bikes;
+
+displayData(data);
+
+function displayData(arr) {
+    arr.map(element => {
+        let super_div = document.createElement("div");
+        let img_container = document.createElement("div");
+        let img = document.createElement("img");
+        let span = document.createElement("span");
+        let hr1 = document.createElement("hr");
+        let img_desc = document.createElement("div");
+        let desc = document.createElement("div");
+        let h3 = document.createElement("h3");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let p3 = document.createElement("p");
+        let hr2 = document.createElement("hr");
+        let view = document.createElement("div");
+
+        img_container.setAttribute("class", "img-container");
+        img.setAttribute("src", element.img);
+        span.textContent = "₹"+element.price;
+        img_desc.setAttribute("class", "img-desc");
+        h3.textContent = element.vehicle_name;        
+        p2.textContent = "Bellanduru (New)";
+        p3.textContent = element.distance + " km away";
+        view.textContent = "QUICK VIEW";
+
+        desc.append(h3, p1, p2, p3);
+        img_desc.append(desc, hr2, view);
+        img_container.append(img, span);
+        super_div.append(img_container, hr1, img_desc);
+
+        if (element.seat == 2) {
+            span.style.top = "200px";
+            p1.textContent = element.transmission_type+", "+element.seat+" Seats, "+element.engine_capacity+"cc";
+        } else {
+            p1.textContent = element.transmission_type+", "+element.seat+" Seats, "+element.fuel_type;
+        }
+
+        document.querySelector(".product-display").append(super_div);
+    });
+}
